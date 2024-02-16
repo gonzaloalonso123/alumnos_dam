afe46f5f17c7764f664c996d494735432c1e25cd
Gonzalo  Alonso <huntymonster@gmail.com>
2024-02-17 00:59:22 +0100

    Nodecook API: Pushing changes.

--- app/database/models/alumnos.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const AlumnosSchema = new mongoose.Schema(
-    {id: "UUID",nombre: "String",apellido: "String",edad: "Number",genero: "String",}
-);
-
-const Alumnos = mongoose.model("Alumnos", AlumnosSchema);
-
-module.exports = Alumnos;
