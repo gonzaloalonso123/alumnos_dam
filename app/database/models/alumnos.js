d5e749f962570023303b639ced0b1272cc1ae078
Gonzalo  Alonso <huntymonster@gmail.com>
2024-02-17 17:52:19 +0100

    remove

--- app/database/models/alumnos.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const AlumnosSchema = new mongoose.Schema(
-    {id: "UUID",nombre: "String",temario: "String",EdaD: "String",}
-);
-
-const Alumnos = mongoose.model("Alumnos", AlumnosSchema);
-
-module.exports = Alumnos;
--- app/database/models/profesores.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const ProfesoresSchema = new mongoose.Schema(
-    {id: "UUID",nombre: "String",}
-);
-
-const Profesores = mongoose.model("Profesores", ProfesoresSchema);
-
-module.exports = Profesores;
