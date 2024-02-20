37e0de0e67f46913d5ab733eee8d47cd4190a45d
Gonzalo  Alonso <huntymonster@gmail.com>
2024-02-20 20:32:44 +0100

    Nodecook API: Pushing changes.

---
+++ app/database/daos/profesores.js
@@ -0,0 +1,35 @@
+const Profesores = require("../models/profesores");
+
+  
+const getAll = async () => {
+  return await Profesores.find();
+};
+
+const getById = async (id) => {
+  return await Profesores.findById(id);
+};
+
+const create = async (data) => {
+  return await Profesores.create(data);
+};
+
+const update = async (id, data) => {
+  return await Profesores.findByIdAndUpdate(id, data, {new: true});
+};
+
+const del = async (id) => {
+  return await Profesores.findByIdAndDelete(id);
+};
+
+const getByNombre = async (data) => {
+    return await Profesores.findOne({ nombre: data }).exec();
+};
+  
+module.exports = {
+  getAll,
+  getById,
+  create,
+  update,
+  del,
+  getByNombre
+};
